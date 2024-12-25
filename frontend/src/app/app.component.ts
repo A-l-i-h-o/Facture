a0757362e6cfa27036e1553920fe8aefb3518027
Maxence Lelandais <misterduflair@gmail.com>
2024-12-25 18:34:27 +0100

    début fomulaire générique

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoggingComponent } from "./pages/logging/logging.component";
-import { HeaderComponent } from "./components/header/header.component";
+import { ReactiveFormsModule } from '@angular/forms'; // Importer ReactiveFormsModule
+import { LoggingComponent } from './pages/logging/logging.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GenericFormComponent } from './components/generic-form/generic-form.component'; // Importer le composant générique
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoggingComponent, HeaderComponent],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    LoggingComponent,
+    HeaderComponent,
+    ReactiveFormsModule, // Ajouter ReactiveFormsModule ici
+    GenericFormComponent // Ajouter le composant générique
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'frontend';
+
+  // Exemple de modèle Person
+  person = {
+    nom: 'Dupont',
+    prenom: 'Jean'
+  };
 }
---
+++ frontend/src/app/model/formulaire/Person.model.ts
@@ -0,0 +1,10 @@
+export class Person {
+    nom: string;
+    prenom: string;
+  
+    constructor(nom: string, prenom: string) {
+      this.nom = nom;
+      this.prenom = prenom;
+    }
+  }
+  
