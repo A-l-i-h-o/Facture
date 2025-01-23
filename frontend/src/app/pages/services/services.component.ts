b81d6461e03d4c34759a80ce676a9be1f393a7e5
Maxence Lelandais <misterduflair@gmail.com>
2025-01-23 16:24:06 +0100

    Ajout liste services

--- frontend/src/app/pages/services/services.component.ts
+++ frontend/src/app/pages/services/services.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FactureService } from 'src/app/http/FactureService';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent {
+export class ServicesComponent  implements OnInit {
 
+  listeServices: any[] = [];
+
+  constructor(private factureService: FactureService,private router: Router) { }
+  ngOnInit(): void {
+    // Récupération des familles depuis l'API au chargement du composant
+    this.factureService.getAllTypeFrais().subscribe(
+      (data) => {
+        this.listeServices = data;
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération des familles', error);
+      }
+    );
+  }
+
+  retour(){
+    this.router.navigate(["accueil"]);
+  }
+
+  archiver(){
+    
+  }
 }
